6f2ab22595ea97e54963bc742fb7f39b743d7375
Aditya goyal <goyaladitya3815@gmail.com>
2024-11-07 20:24:08 +0530

    Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,20 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import my from '../src/assets/images/my.jpg';
 
-
-
 const App = () => {
   return (
-      <div className="bg-zinc-900 h-screen text-white relative">
-        <img 
-          src={my} 
-          alt="logo" 
-          className="absolute top-0 left-0 w-full h-screen opacity-10" 
-        />
-        <div className="relative z-10">
-          <Navbar />
-        </div>
-        <Hero/>
-        <About /> 
+    <div className="bg-zinc-900 min-h-screen text-white relative">
+      <img 
+        src={my} 
+        alt="logo" 
+        className="absolute top-0 left-0 w-full min-h-screen object-cover opacity-20" 
+      />
+      <div className="relative z-10">
+        <Navbar />
       </div>
+      <Hero />
+      <About/>
+    </div>
   );
 };
 
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,9 @@
+import React from 'react'
+
 const About = () => {
-    return (
-      <div className="z-0 p-8 text-white">
-        <div>
-          Hi i am Aditya goyal 
-        </div>
-      </div>
-    );
-  };
-  
-  export default About;
-  
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
