64a927e34cb17500d76a5aa0271db61e5a017c05
Aditya goyal <goyaladitya3815@gmail.com>
2024-11-08 21:43:12 +0530

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,38 @@
 import Navbar from "./components/Navbar";
-import About from "./components/About"; 
+import About from "./components/About";
 import Hero from "./components/Hero";
-import my from '../src/assets/images/my.jpg';
+import Services from "./components/Services"; 
+import Contact from "./components/ContactUs"; 
+import my from '../src/assets/images/Hero.jpg';
+import { Element } from 'react-scroll'; 
 
 const App = () => {
   return (
-    <div className="bg-zinc-900 min-h-screen text-white relative">
-      <img 
-        src={my} 
-        alt="logo" 
-        className="absolute top-0 left-0 w-full min-h-screen object-cover opacity-20" 
-      />
+    <div className="bg-zinc-900 min-h-screen text-white relative w-full">
+      {/* Background Image with opacity */}
+      <div className="absolute top-0 left-0 w-full h-full bg-cover bg-fixed opacity-10"
+        style={{ backgroundImage: `url(${my})` }} ></div>
+      
+      {/* Navbar */}
       <div className="relative z-10">
         <Navbar />
       </div>
-      <Hero />
-      <About/>
+      
+      {/* Main Content */}
+      <div className="relative z-10">
+        <Element name="home">
+          <Hero />
+        </Element>
+        <Element name="about">
+          <About />
+        </Element>
+        <Element name="services">
+          <Services />
+        </Element>
+        <Element name="contact">
+          <Contact />
+        </Element>
+      </div>
     </div>
   );
 };
