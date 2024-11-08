0511c134ac425d523ef90fb7bffb19eb70c67888
Aditya goyal <goyaladitya3815@gmail.com>
2024-11-08 22:48:45 +0530

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,34 @@ import Services from "./components/Services";
 import Contact from "./components/ContactUs"; 
 import my from '../src/assets/images/Hero.jpg';
 import { Element } from 'react-scroll'; 
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
-    <div className="bg-zinc-900 min-h-screen text-white relative w-full">
-      <div className="absolute top-0 left-0 w-full h-full bg-cover bg-fixed opacity-10"
-        style={{ backgroundImage: `url(${my})` }} ></div>
-      <div className="relative z-10">
-        <Navbar />
+    <>
+      <Analytics />
+      <div className="bg-zinc-900 min-h-screen text-white relative w-full">
+        <div className="absolute top-0 left-0 w-full h-full bg-cover bg-fixed opacity-10"
+          style={{ backgroundImage: `url(${my})` }}></div>
+        <div className="relative z-10">
+          <Navbar />
+        </div>
+        <div className="relative z-10">
+          <Element name="home">
+            <Hero />
+          </Element>
+          <Element name="about">
+            <About />
+          </Element>
+          <Element name="services">
+            <Services />
+          </Element>
+          <Element name="contact">
+            <Contact />
+          </Element>
+        </div>
       </div>
-      <div className="relative z-10">
-        <Element name="home">
-          <Hero />
-        </Element>
-        <Element name="about">
-          <About />
-        </Element>
-        <Element name="services">
-          <Services />
-        </Element>
-        <Element name="contact">
-          <Contact />
-        </Element>
-      </div>
-    </div>
+    </>
   );
 };
 
